52e4e9a07d24436aa7589176f84caa907214dd33
Sebastian Quintero <sebastian.quinteroo@uqvirtual.edu.co>
2022-05-24 16:41:09 -0500

    Solucion ramdon sonar

--- negocio/src/main/java/co/edu/uniquindio/proyecto/servicios/CompraServicioImpl.java
+++ negocio/src/main/java/co/edu/uniquindio/proyecto/servicios/CompraServicioImpl.java
@@ -74,7 +74,9 @@ public class CompraServicioImpl implements CompraServicio, Serializable {
 
         if (c!=null) {
 
-            int codEnvio= (int) (10000 + Math.random() * 90000);
+            Random ran = new Random();
+
+            int codEnvio= (10000 + ran.nextInt() * 90000);
             Envio e = new Envio();
             List<Compra> compras = new ArrayList<>();
             LocalDate dateSend = LocalDate.now();
